cdd4022401c0cafefdefe24cfa168c6d1c2a2e89
Bence <github@spitkov.hu>
2025-04-02 23:15:21 +0200

    apr1 vege

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
     import DiscordStatus from '$lib/components/DiscordStatus.svelte';
     
     const name = "spitkov";
-    const title = "full-stack dev & cybersec & snecológus";
+    const title = "full-stack dev & cybersec";
     const profilePic = "https://x.spitkov.hu/raw/aMSOMT.png";
     const email = "bence@spitkov.hu";
     const github = "spitkov";
